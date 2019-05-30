a9e2fd121ba8edfa0696d2e310c25a1e7d696dd7
Tatenda <tjmakunike@gmail.com>
2019-05-30 10:26:56 +0200

    site structure complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServicesComponent } from './components/services/services.component';
+import { TeamComponent } from './components/team/team.component';
+import { SocialComponent } from './components/social/social.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'home', redirectTo: '' },
+  { path: 'about-us', component: AboutComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'team', component: TeamComponent },
+  { path: 'testimonials', component: SocialComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { AboutComponent } from './components/about/about.component';
+import { ServicesComponent } from './components/services/services.component';
+import { TeamComponent } from './components/team/team.component';
+import { SocialComponent } from './components/social/social.component';
+import { ContactComponent } from './components/contact/contact.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LandingPageComponent,
+        AboutComponent,
+        ServicesComponent,
+        TeamComponent,
+        SocialComponent,
+        ContactComponent
+    ],
+    imports: [
+        BrowserModule.withServerTransition({ appId: 'serverApp' }),
+        AppRoutingModule,
+        CarouselModule.forRoot()
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
